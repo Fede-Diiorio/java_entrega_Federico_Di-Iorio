ceacdc665441d6d466c894f286d1390d9e5cff6a
Federico Di Iorio <federico_metallica@hotmail.com>
2024-10-29 17:46:03 -0300

    Se migró el código para agregar productos a un archivo sql

--- src/main/java/com/coderhouse/EntregaCoderApplication.java
+++ src/main/java/com/coderhouse/EntregaCoderApplication.java
@@ -1,11 +1,9 @@
 package com.coderhouse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.coderhouse.helpers.DatabaseLoader;
 
 @SpringBootApplication
 public class EntregaCoderApplication implements CommandLineRunner{
@@ -14,12 +12,9 @@ public class EntregaCoderApplication implements CommandLineRunner{
 		SpringApplication.run(EntregaCoderApplication.class, args);
 	}
 	
-	@Autowired DatabaseLoader dbl;
 
 	@Override
 	public void run(String... args) throws Exception {
-		dbl.loadCategories();
-		dbl.loadProducts();
 	}
 
 }
