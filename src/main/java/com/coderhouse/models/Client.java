fd1c0b9c0f10473977ccf897124430f04fa31c50
Federico Di Iorio <federico_metallica@hotmail.com>
2024-11-12 19:40:22 -0300

    Se agregó el archivo jar

--- src/main/java/com/coderhouse/models/Client.java
+++ src/main/java/com/coderhouse/models/Client.java
@@ -3,6 +3,7 @@ package com.coderhouse.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -25,28 +26,35 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Schema(description = "Modelo de cliente")
 @Entity
 @Table(name = "clients")
 public class Client {
 
 	// Attributes
+	@Schema(description = "ID del cliente", requiredMode = Schema.RequiredMode.REQUIRED)
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
+	@Schema(description = "Nombre del cliente", requiredMode = Schema.RequiredMode.REQUIRED)
 	@Column(nullable = false, length = 75)
 	private String name;
 
+	@Schema(description = "Apellido del cliente", requiredMode = Schema.RequiredMode.REQUIRED)
 	@Column(nullable = false, length = 75)
 	private String lastname;
 
+	@Schema(description = "Documento del cliente", requiredMode = Schema.RequiredMode.REQUIRED)
 	@Column(nullable = false, unique = true, length = 11)
 	private String docnumber;
 
+	@Schema(description = "ID del carrito del cliente", requiredMode = Schema.RequiredMode.REQUIRED)
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "cart_id", nullable = false)
 	private Cart cart;
 
+	@Schema(description = "Lista de Tickets")
 	@OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<Ticket> tickets = new ArrayList<Ticket>();
 
