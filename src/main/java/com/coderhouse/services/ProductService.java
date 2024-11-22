c0bc47fef124cae6a19a4543d1955b0a6867f5ff
Federico Di Iorio <federico_metallica@hotmail.com>
2024-11-22 07:19:32 -0300

    Cambios por errores de compatibilidad entre ramas

--- src/main/java/com/coderhouse/services/ProductService.java
+++ src/main/java/com/coderhouse/services/ProductService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.coderhouse.dto.ProductDTO;
+import com.coderhouse.dtos.ProductDTO;
 import com.coderhouse.models.Product;
 import com.coderhouse.models.Category;
 import com.coderhouse.repositories.CategoryRepository;
